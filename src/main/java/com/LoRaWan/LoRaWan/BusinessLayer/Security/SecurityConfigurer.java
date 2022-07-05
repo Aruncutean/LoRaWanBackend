d5224de7540d9460fb7fba3639033fefd7748a5a
Aruncutean <aruncutean.raul12@gmail.com>
2022-07-05 16:17:38 +0300

    Refactor

--- src/main/java/com/LoRaWan/LoRaWan/BusinessLayer/Security/SecurityConfigurer.java
+++ src/main/java/com/LoRaWan/LoRaWan/BusinessLayer/Security/SecurityConfigurer.java
@@ -44,7 +44,6 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers("/user/addUser").permitAll()
                 .antMatchers("/node/getAllNode").permitAll()
                 .antMatchers("/node/getData/{name}").permitAll()
-                .antMatchers("/node/deleteNode/{name}").permitAll()
                 .antMatchers("/message/addMessage").permitAll()
                 .antMatchers("/date/getLast7Day/{name}").permitAll()
                 .antMatchers("/date/getLastMonth/{name}").permitAll()
